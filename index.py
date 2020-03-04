9f7c79fe1edb434424c260095f9efe7a0ae2d7f8
jemsppl <jemsheer@meaec.edu.in>
2020-03-04 18:41:01 +0530

    first day

--- index.py
+++ index.py
@@ -1,3 +1,45 @@
 print ("Hello World")
-if 5 > 4:
-    print ("5 is greater than 4");
+x = 5
+y = "This is a string";
+# This is a comment section 
+print (x);
+print ("This is a comment section checking code") #THis is a comment
+"""
+This is a multi line comment section
+THis is its second line
+This is third line
+
+"""
+
+a = "First Name";
+b = "Second Name";
+c = a + b;
+print (c);
+print (type(c));
+#example for list
+list = ["Sunday","Monday","Tuesday","Thirsday"]
+#example for tuple
+tuple = ("apple","orange","banana","grapes")
+#example for erange
+print (type(list));
+x = frozenset({"apple","orange","cherry"})
+print (x)
+x = b"Hellp"
+print (x)
+a = "Hello"
+b = 5
+
+print (bool(a))
+print (5+5**2)
+print (2<<2)
+list_x = ["zeroth:0","1","2","3","4","5","6","7"]
+list_x.pop()
+print (list_x)
+list_x.append("5")
+print (list_x)
+
+list1 = ["Sunday","Monday","Tuesday","Thirsday"]
+list2 = ["1","2","3","4","5"]
+
+list1.append(list2)
+print (list1)
