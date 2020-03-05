9cd7b02f961bdb7bf02586ffb5e7d503f3cd5f96
jemsppl <jemsheer@meaec.edu.in>
2020-03-05 07:17:15 +0530

    after from pycharm:

--- index.py
+++ index.py
@@ -1,45 +1,38 @@
-print ("Hello World")
-x = 5
-y = "This is a string";
-# This is a comment section 
-print (x);
-print ("This is a comment section checking code") #THis is a comment
-"""
-This is a multi line comment section
-THis is its second line
-This is third line
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
 
-"""
+print(x) 
 
-a = "First Name";
-b = "Second Name";
-c = a + b;
-print (c);
-print (type(c));
-#example for list
-list = ["Sunday","Monday","Tuesday","Thirsday"]
-#example for tuple
-tuple = ("apple","orange","banana","grapes")
-#example for erange
-print (type(list));
-x = frozenset({"apple","orange","cherry"})
-print (x)
-x = b"Hellp"
-print (x)
-a = "Hello"
-b = 5
+for x in range(10):
+    print ("This is ", x)
 
-print (bool(a))
-print (5+5**2)
-print (2<<2)
-list_x = ["zeroth:0","1","2","3","4","5","6","7"]
-list_x.pop()
-print (list_x)
-list_x.append("5")
-print (list_x)
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
 
-list1 = ["Sunday","Monday","Tuesday","Thirsday"]
-list2 = ["1","2","3","4","5"]
+for x in adj:
+  for y in fruits:
+    print(x, y)
 
-list1.append(list2)
-print (list1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
